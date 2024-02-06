15140f07c02ac9c28c9cc9e6aec5385b13a4d1d2
Jaisurya S <86350249+Jaisurya48@users.noreply.github.com>
2024-02-06 11:25:53 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class WhileLoop{
+    public static void main(String[] args) {
+        int i=0;
+        while(i<=48){
+            System.out.println(i);
+            i++; //increment operator (j++) = (j+=1)
+        }
+        System.out.println("Descending order of numbers from 48");
+        int j=48;
+        while(j>0){
+            System.out.println(j);
+            j--; //decrement operator (j--) = (j-=1)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
