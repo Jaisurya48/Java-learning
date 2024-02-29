462e456a35c4259719639d2374dbfb3cc87cf694
Jaisurya S <86350249+Jaisurya48@users.noreply.github.com>
2024-02-29 15:46:05 +0530

    Update CountEvenAndOddNumber.java

--- CountEvenAndOddNumber.java
+++ CountEvenAndOddNumber.java
@@ -1,6 +1,22 @@
 import java.util.*;
 class CountEvenAndOddNumber{
     public static void main(String[]args){
+        System.out.println("Enter the Number:");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
 
+        int even_count=1;
+        int odd_count=1;
+        
+        while(num>0){
+            int rem=num%10;
+        if(rem%2==0){
+            System.out.println("The Even digit count is:"+even_count++);
+        }
+        else{
+            System.out.println("The odd digit count is:"+odd_count++);
+        }
+        num=num/10;
+    }
     }
 }
